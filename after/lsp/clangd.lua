b0ce3704b321263b19a0003ca46fada5723f123e
HaizorWill <thelines6@gmail.com>
2025-12-22 01:26:12 +0300

    Add clangd conf 2?

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -1,1 +1,16 @@
+return {
+  cmd = {
+    "clangd",
+    -- "--background-index",
+    -- "-j=12",
+    -- "--clang-tidy",
+    -- "--clangd-tidy-checks=*",
+    -- "--all-scopes-completion",
+    -- "--completion-style-detailed",
+  },
+  init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
     clangdFileStatus = true,
+  },
+}
