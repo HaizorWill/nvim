b9ff2d0c9c9c072ac685c98b99e4288a11a3c59e
HaizorWill <thelines6@gmail.com>
2026-02-16 20:15:03 +0300

    Add macro status to lualine; change position of some thing
    I don't know, don't question me

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,8 +4,28 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
+        always_show_tabline = false,
         globalstatus = true,
       },
+      sections = {
+        lualine_c = { "filename" },
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+          },
+        },
+        lualine_y = {
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_z = {
+          "progress",
+          "location",
+        },
+      },
+      tabline = {},
     },
   },
 }
